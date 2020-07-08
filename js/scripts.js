02951357d14ab828b226b15039c6a33a8e04ae4e
Vanessa Guerrero <vamarilug@gmail.com>
2020-07-07 22:58:40 -0700

    Add submit button to form, create checkboxes for two items on the list, and add function for creation of grocery list array to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-
+$("#ingredients").submit(function(event) {
   event.preventDefault();
+
+  let item=[".peanuts", ".sprouts"];
+  let groceries=[];
+  groceries.push($("input#peanuts").val());
+  groceries.push($("input#sprouts").val());
 
 });
 });
